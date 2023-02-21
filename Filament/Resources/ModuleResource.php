460830f3abb1b77afddac81b499ffe15e8ab9ef1
Sorethea OK <sorethea@gmail.com>
2023-02-21 13:50:16 +0700

    lam-dev

--- Filament/Resources/ModuleResource.php
+++ Filament/Resources/ModuleResource.php
@@ -7,10 +7,8 @@ use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
 use Filament\Tables\Actions\Action;
-use Modules\Core\Classes\CoreModule;
-use Modules\Core\Filament\Resources\ModuleResource\Pages;
-use Modules\Core\Filament\Resources\ModuleResource\RelationManagers;
-use Modules\Core\Models\Module;
+use Modules\LAM\Models\Module;
+
 
 class ModuleResource extends Resource
 {
@@ -48,77 +46,77 @@ class ModuleResource extends Resource
             ->actions([
                 //Tables\Actions\EditAction::make(),
 
-                Action::make('enable')
-                    ->requiresConfirmation()
-                    ->modalHeading(fn($record)=>"Enable {$record->name} Module")
-                    ->action(function ($record){
-                        $module = \Module::find($record->name);
-                        $module->enable();
-                        redirect(request()->header("Referer"));
-                    })
-                    ->color("success")
-                    ->icon('heroicon-o-eye')
-                    ->size('lg')
-                    ->iconButton()
-                    ->visible(function($record){
-                        $module = \Module::find($record->name);
-                        return auth()->user()->can("modules.manager")
-                            && !\Core::isCore($record->name)
-                            && !$module->isEnabled()
-                            && $record->installed;
-                    }),
-                Action::make('disable')
-                    ->requiresConfirmation()
-                    ->modalHeading(fn($record)=>"Disable {$record->name} Module")
-                    ->action(function ($record){
-                        $module = \Module::find($record->name);
-                        $module->disable();
-                        redirect(request()->header("Referer"));
-                    })
-                    ->icon('heroicon-o-eye-off')
-                    ->size('lg')
-                    ->iconButton()
-                    ->color("warning")
-                    ->visible(function($record){
-                        $module = \Module::find($record->name);
-                        return auth()->user()->can("modules.manager")
-                            && !\Core::isCore($record->name)
-                            && $module->isEnabled()
-                            && $record->installed;
-                    }),
-                Action::make('installation')
-                    ->requiresConfirmation()
-                    ->modalHeading()
-                    ->iconButton()
-                    ->icon('heroicon-o-download')
-                    ->size('lg')
-                    ->color('danger')
-                    ->visible(function ($record):bool{
-                        return !\Core::isCore($record->name) && !$record->installed && auth()->user()->can("modules.manager");
-                    })
-                    ->action(function ($record){
-                        $coreModule = new CoreModule($record->name);
-                        $coreModule->install();
-                        //$record->installed = true;
-                        $record->save();
-                        redirect(request()->header("Referer"));
-                    }),
-                Action::make("uninstallation")
-                    ->modalHeading()
-                    ->iconButton()
-                    ->icon('heroicon-o-trash')
-                    ->size('lg')
-                    ->color('danger')
-                    ->visible(function ($record):bool{
-                        return !\Core::isCore($record->name) && $record->installed && auth()->user()->can("modules.manager");
-                    })
-                    ->action(function($record){
-                        \Core::uninstall($record->name);
-                        $record->installed = false;
-                        $record->save();
-                        redirect(request()->header("Referer"));
-                    })
-                    ->requiresConfirmation(),
+//                Action::make('enable')
+//                    ->requiresConfirmation()
+//                    ->modalHeading(fn($record)=>"Enable {$record->name} Module")
+//                    ->action(function ($record){
+//                        $module = \Module::find($record->name);
+//                        $module->enable();
+//                        redirect(request()->header("Referer"));
+//                    })
+//                    ->color("success")
+//                    ->icon('heroicon-o-eye')
+//                    ->size('lg')
+//                    ->iconButton()
+//                    ->visible(function($record){
+//                        $module = \Module::find($record->name);
+//                        return auth()->user()->can("modules.manager")
+//                            && !\Core::isCore($record->name)
+//                            && !$module->isEnabled()
+//                            && $record->installed;
+//                    }),
+//                Action::make('disable')
+//                    ->requiresConfirmation()
+//                    ->modalHeading(fn($record)=>"Disable {$record->name} Module")
+//                    ->action(function ($record){
+//                        $module = \Module::find($record->name);
+//                        $module->disable();
+//                        redirect(request()->header("Referer"));
+//                    })
+//                    ->icon('heroicon-o-eye-off')
+//                    ->size('lg')
+//                    ->iconButton()
+//                    ->color("warning")
+//                    ->visible(function($record){
+//                        $module = \Module::find($record->name);
+//                        return auth()->user()->can("modules.manager")
+//                            && !\Core::isCore($record->name)
+//                            && $module->isEnabled()
+//                            && $record->installed;
+//                    }),
+//                Action::make('installation')
+//                    ->requiresConfirmation()
+//                    ->modalHeading()
+//                    ->iconButton()
+//                    ->icon('heroicon-o-download')
+//                    ->size('lg')
+//                    ->color('danger')
+//                    ->visible(function ($record):bool{
+//                        return !\Core::isCore($record->name) && !$record->installed && auth()->user()->can("modules.manager");
+//                    })
+//                    ->action(function ($record){
+//                        $coreModule = new CoreModule($record->name);
+//                        $coreModule->install();
+//                        //$record->installed = true;
+//                        $record->save();
+//                        redirect(request()->header("Referer"));
+//                    }),
+//                Action::make("uninstallation")
+//                    ->modalHeading()
+//                    ->iconButton()
+//                    ->icon('heroicon-o-trash')
+//                    ->size('lg')
+//                    ->color('danger')
+//                    ->visible(function ($record):bool{
+//                        return !\Core::isCore($record->name) && $record->installed && auth()->user()->can("modules.manager");
+//                    })
+//                    ->action(function($record){
+//                        \Core::uninstall($record->name);
+//                        $record->installed = false;
+//                        $record->save();
+//                        redirect(request()->header("Referer"));
+//                    })
+//                    ->requiresConfirmation(),
 //                DeleteAction::make()
 //                    ->icon(false)
 //                    ->button()
