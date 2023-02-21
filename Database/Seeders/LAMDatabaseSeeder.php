7af5c8caaa11859ba4784ca59773b2ccbe8213b5
Sorethea OK <sorethea@gmail.com>
2023-02-21 08:28:53 +0700

    First Commit

---
+++ Database/Seeders/LAMDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\LAM\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class LAMDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('lam::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('lam.name') !!}
+    </p>
+@endsection
