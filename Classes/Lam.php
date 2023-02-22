e5c60e97a2efaf608bf1f3e2c56c193f59e61671
Sorethea OK <sorethea@gmail.com>
2023-02-22 14:13:59 +0700

    lam-dev

--- Classes/Lam.php
+++ Classes/Lam.php
@@ -6,26 +6,26 @@ use Modules\LAM\Models\Module;
 
 abstract class Lam
 {
-    public static function getModuleNamespace(){
+    public function getModuleNamespace(){
         return config("modules.namespace","Modules");
     }
-    public static function getModuleProviderPath(){
+    public function getModuleProviderPath(){
         return config("modules.paths.generator.provider.path","Providers");
     }
-    public static function getModuleProviderNamespace($moduleName) :string{
+    public function getModuleProviderNamespace($moduleName) :string{
         return self::getModuleNamespace()."\\".$moduleName."\\".self::getModuleProviderPath();
     }
-    public static function setInstalled(Module $module,bool $installed): void
+    public function setInstalled(Module $module,bool $installed): void
     {
         $module->json()->set("installed",$installed)->save();
     }
-    public static function isInstalled($module):bool{
+    public function isInstalled($module):bool{
         return $module->json()->get("installed",false);
     }
-    public static function isSystem($module):bool{
+    public function isSystem($module):bool{
         return $module->json()->get("type","module")=="system";
     }
-    public static function isVisibleForEnable($name) :bool
+    public function isVisibleForEnable($name) :bool
     {
         $module = \Module::find($name);
         return
@@ -34,7 +34,7 @@ abstract class Lam
             self::isInstalled($module) &&
             !self::isSystem($module);
     }
-    public static function isVisibleForDisable($name) :bool
+    public function isVisibleForDisable($name) :bool
     {
         $module = \Module::find($name);
         return
@@ -43,7 +43,7 @@ abstract class Lam
             !self::isSystem($module) &&
             self::isInstalled($module);
     }
-    public static function isVisibleForInstall($name): bool
+    public function isVisibleForInstall($name): bool
     {
         $module = \Module::find($name);
         return
@@ -51,7 +51,7 @@ abstract class Lam
             !self::isSystem($module) &&
             !self::isInstalled($module);
     }
-    public static function isVisibleForUninstall($name): bool
+    public function isVisibleForUninstall($name): bool
     {
         $module = \Module::find($name);
         return
@@ -60,7 +60,7 @@ abstract class Lam
             !$module->isEnabled() &&
             self::isInstalled($module);
     }
-    public static function install($name): Module
+    public function install($name): Module
     {
         $module = \Module::find($name);
         \Artisan::call("module:migrate-refresh ".$module->getName());
@@ -69,7 +69,7 @@ abstract class Lam
         self::setInstalled($module,true);
         return $module;
     }
-    public static function uninstall($name){
+    public function uninstall($name){
         $module = \Module::find($name);
         \Artisan::call("module:migrate-rollback ".$module->getName());
         app()->register(self::getModuleProviderNamespace($module->getName())."\\UninstallServiceProvider");
@@ -78,7 +78,7 @@ abstract class Lam
         return $module;
     }
 
-    public static function scan(): void
+    public function scan(): void
     {
         Module::truncate();
         $modules = \Module::all();
