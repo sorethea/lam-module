98d7590858566526220e6d0c0cd2a00d8e799614
Sorethea OK <sorethea@gmail.com>
2023-02-27 08:40:21 +0700

    lam-dev

--- Providers/AuthServiceProvider.php
+++ Providers/AuthServiceProvider.php
@@ -3,10 +3,14 @@
 namespace Modules\LAM\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Modules\LAM\Models\Module;
 use Modules\LAM\Models\User;
+use Modules\LAM\Policies\ActivityPolicy;
+use Modules\LAM\Policies\ModulePolicy;
 use Modules\LAM\Policies\PermissionPolicy;
 use Modules\LAM\Policies\RolePolicy;
 use Modules\LAM\Policies\UserPolicy;
+use Spatie\Activitylog\Models\Activity;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -16,7 +20,9 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies=[
         User::class => UserPolicy::class,
         Permission::class => PermissionPolicy::class,
-        Role::class => RolePolicy::class
+        Role::class => RolePolicy::class,
+        Module::class => ModulePolicy::class,
+        Activity::class => ActivityPolicy::class,
     ];
 
     /**
